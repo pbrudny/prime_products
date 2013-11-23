617a578f9c587e96d7f742b68208b2da6c3c9142
Piotr Brudny <pbrudny@gmail.com>
2013-11-23 23:43:10 +0000

    Removed 1 from the first cell

--- lib/multiplication_table.rb
+++ lib/multiplication_table.rb
@@ -7,12 +7,17 @@ require 'lib/table'
 class MultiplicationTable < Table
   attr_reader :vector
 
-  def initialize(values)
+  def initialize(values, options = {})
     @vector = create_vector(values)
-    super(multiplication_matrix.to_a)
+    super(multiplication_array(options))
   end
 
   private
+
+  def empty_first_cell(array)
+    array[0][0] = '--'
+    array
+  end
 
   def create_vector(values_array)
     Vector.elements(values_array)
@@ -24,6 +29,15 @@ class MultiplicationTable < Table
   # Note: It returns Matrix not Array.
   def multiply_vectors(vector1, vector2 = vector1)
     vector1 * vector2.covector
+  end
+
+  def multiplication_array(options)
+    table = multiplication_matrix.to_a
+    if options[:empty_first_cell]
+      empty_first_cell table
+    else
+      table
+    end
   end
 
   def multiplication_matrix
--- prime_products.rb
+++ prime_products.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'lib/multiplication_table'
 require 'lib/prime_generator'
 
+#
 # Display table with products of the first 10 prime numbers.
-puts MultiplicationTable.new([1] + PrimeGenerator.new(10).primes).formatted
+# Note: [1] is used for multiplication but is not displayed because it is not a prime number.
+#
+puts MultiplicationTable.new([1] + PrimeGenerator.new(10).primes, :empty_first_cell => true).formatted
 
